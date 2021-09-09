8664650adad44de67eff68add2b51187e86b0d2b
Dirghayu <dirghayu1999@gmail.com>
2021-09-09 13:53:52 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  application:
+  {
+    name: 'angular-starter',
+    angular: 'Angular 12.2.0',
+    bootstrap: 'Bootstrap 5.1.0',
+    fontawesome: 'Font Awesome 5.15.4',
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  application:
+  {
+    name: 'angular-starter',
+    angular: 'Angular 12.2.0',
+    bootstrap: 'Bootstrap 5.1.0',
+    fontawesome: 'Font Awesome 5.15.4',
+  }
+};
